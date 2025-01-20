14b7979bdfaf95fb222be3385806cf2cbe3655fc
gustavolimadesousa <gustavolimadesousa@hotmail.com>
2025-01-20 12:17:43 +0000

    update rebuild code

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -240,3 +240,14 @@ VALUES   (
   );
 
 
+-- 4 - Modify the "GM Hummer" record to read "a huge interior" rather than "small interiors" using a single query.
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 6 - Update all records in the inventory table to add "/vehicles" to the middle of the file path in the inv_image and inv_thumbnail columns using a single query.
+UPDATE public.inventory
+SET 
+    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
